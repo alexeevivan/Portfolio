2f977378f29555ca9811e9accf29fabdbe9a7cc5
Ivan Alexeev <alexeevivan92@gmail.com>
2022-12-07 09:13:30 +0300

    adding new fonts to the project

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -1,10 +1,23 @@
 import * as flsFunctions from "./modules/functions.js";
-
 flsFunctions.isWebp();
-
 
 window.addEventListener("scroll", () => {
 	document.body.style.setProperty("--scroll", window.pageYOffset / window.innerHeight);
 },
 	false
 );
+
+// Preloader
+$.get("../html/loader.html", function (data) {
+	$("#loading").replaceWith(data);
+});
+
+$(window).on('load', function () {
+	setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader() {
+	$(".loader-wrapper").fadeOut(1000, function () {
+		// fadeOut complete. Remove the loading div
+		$(".loader-wrapper").remove(); //makes page more lightweight 
+	});
+}
